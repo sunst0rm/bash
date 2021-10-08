b2387c4d38fbf865adeb7dfae5428d56f6b04a55
Jarosław Kozioł <jaroslaw.koziol@inteliwise.com>
2021-10-08 14:42:39 +0200

    fixed path

--- copyfirefoxprofile.sh
+++ copyfirefoxprofile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# This is a dummy script to use with cron, which copies Firefox profile to another location.
+# This is a dummy script to use with cron, which copies Firefox profile to another location on Mac.
 #
 # My purpose is to avoid errors during backup using rsync, cause it does not see properly
 # Firefox profile's path and gives error at 'Application\ Support/'
+#
 
-cd /Users/jarek/Library/Application\ Support/Firefox
-cp -R /Users/jarek/Library/Application\ Support/Firefox /Users/jarek/Documents/Profile/
+cd /Users/jarek/Library/Application\ Support/Firefox/Profiles || exit
+cp -R /Users/jarek/Library/Application\ Support/Firefox/Profiles /Users/jarek/Documents/Profiles
